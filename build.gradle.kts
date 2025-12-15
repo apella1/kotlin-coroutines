4c95e234b60daf4a8a3f97cd6c1a57febf746eea
John Apella <johncollons5@gmail.com>
2025-12-15 16:52:58 +0300

    use runBlocking and nested launch coroutines

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,9 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.7.3")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     testImplementation(kotlin("test"))
 }
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,30 @@
 package me.apella
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.yield
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+    // hierarchy of coroutines -> runBlocking creates nested launch coroutines
+    runBlocking {
+        launch {
+            println("Sledge: Suplex")
+            tagOut()
+            println("Sledge: F4 Leglock")
+            tagOut()
+            println("Sledge: Pinning 1,2,3")
+        }
+
+        launch {
+            println("Hammer: Clothesline")
+            tagOut()
+            println("Hammer: Piledriver")
+        }
+
     }
+}
+
+suspend fun tagOut() {
+    println("Tagging out!")
+    yield()
 }
